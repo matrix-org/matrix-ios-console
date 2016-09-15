d9a094a16ec597cfd0d0d5afb47e985e89a7132d
giomfo <giom@matrix.org>
2016-09-15 10:40:25 +0200

    Local contacts: Refresh local contacts when app is resumed only when the contacts access is determined.

--- matrixConsole/AppDelegate.m
+++ matrixConsole/AppDelegate.m
@@ -231,6 +231,9 @@
         [[NSUserDefaults standardUserDefaults] registerDefaults:defaults];
         [[NSUserDefaults standardUserDefaults] synchronize];
         
+        // Configure local contacts management
+        [MXKContactManager sharedManager].enableFullMatrixIdSyncOnLocalContactsDidLoad = NO;
+        
         // Add matrix observers, and initialize matrix sessions if the app is not launched in background.
         [self initMatrixSessions];
     }
@@ -350,9 +353,12 @@
         [account resume];
     }
     
-    // refresh the contacts list
-    [MXKContactManager sharedManager].enableFullMatrixIdSyncOnLocalContactsDidLoad = NO;
-    [[MXKContactManager sharedManager] loadLocalContacts];
+    // Check if the application is allowed to access the local contacts
+    if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusAuthorized)
+    {
+        // Refresh the local contacts list by reloading it
+        [[MXKContactManager sharedManager] loadLocalContacts];
+    }
     
     _isAppForeground = YES;
 }
